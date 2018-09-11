451097c6a8a82f3bce2d54178648c0397ec27c2d
Ivan Ivanoff <iivanoff@thedmsgrp.com>
2018-09-11 13:12:20 -0400

    Adding babel to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     filename: 'embed.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
