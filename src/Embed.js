0eaf9bc5cc21c64f0773cde7f873abe422d6c59c
Ivan Ivanoff <iivanoff@thedmsgrp.com>
2018-11-20 12:01:36 -0500

    Make embed point more reliable

--- src/Embed.js
+++ src/Embed.js
@@ -18,8 +18,7 @@ class Embed
     constructor(config)
     {
         this.config = config;
-        this.createEmbedElement();
-        this.form = new Form(this.config);
+        this.createEmbedElement().then(() => new Form(this.config));
     }
 
     /**
@@ -27,14 +26,31 @@ class Embed
      */
     createEmbedElement()
     {
-        let scripts = document.getElementsByTagName('script');
-        let currentScriptElement = scripts[scripts.length - 1];
-        let scriptElement = currentScriptElement.previousElementSibling;
-        this.endpointBase = this.url || this.getScriptSource(scriptElement);
-        let element = document.createElement('div');
-        element.id = 'studio';
-        this.addSpinner(element);
-        currentScriptElement.parentNode.insertBefore(element, currentScriptElement.nextSibling);
+        return new Promise((resolve) => {
+            this.getCurrentScript().then((script) => {
+                this.endpointBase = this.url || this.getScriptSource(script.previousElementSibling);
+                let element = document.createElement('div');
+                element.id = 'studio';
+                script.parentNode.insertBefore(element, script.nextSibling);
+                resolve(element);
+            });
+        });
+    }
+
+    getCurrentScript()
+    {
+        return new Promise((resolve) => {
+            if (document.currentScript) {
+                resolve(document.currentScript);
+            }
+            let scripts = document.getElementsByTagName('script');
+            // Provide a fallback if our script was loaded asynchronously
+            for (let key in scripts) {
+                scripts[key].src && scripts[key].src.includes('/forms/dist/v2.js') && resolve(scripts[key]);
+            }
+            // Final fallback
+            resolve(scripts[scripts.length - 1]);
+        });
     }
 
     addSpinner(element)
