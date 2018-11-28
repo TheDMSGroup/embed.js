11efce0a17e187196afbdde401be50331d07e4ae
wwatson <wwatson@thedmsgrp.com>
2018-11-28 16:42:15 -0500

    nextButton instead of nextPage

--- src/components/overrides/Select.js
+++ src/components/overrides/Select.js
@@ -11,13 +11,14 @@ export default class SubmitSelectComponent extends SelectComponent {
         this.addEventListener(input, 'change', () => {
             const inputTypes = ['checkbox', 'phoneNumber', 'textfield', 'textarea', 'datetime', 'number', 'email', 'select', 'selectboxes', 'signature', 'time', 'file', 'day', 'address', 'password', 'radio', 'currency'];
             const myId = this.id;
-            let lastInputIndex = 0, myIndex = 0;
-            for (let xIndex in this.root.components) {
-                if (inputTypes.includes(this.root.components[xIndex].type)) lastInputIndex = xIndex;
-                if (myId == this.root.components[xIndex].id) myIndex = xIndex;
+            let lastInputIndex = null, myIndex = null;
+            console.log(this);
+            for (let xIndex in this.parent.components) {
+                if (inputTypes.includes(this.parent.components[xIndex].type)) lastInputIndex = xIndex;
+                if (myId == this.parent.components[xIndex].id) myIndex = xIndex;
             }
-            if (myIndex == (this.root.components.length - 1) || myIndex == lastInputIndex) {
-                this.root.nextPage();
+            if (myIndex == (this.parent.components.length - 1) || myIndex == lastInputIndex) {
+                this.emit('nextButton', { data: this.data });
             }
         });
     }
