ac0456f4a66ca6a9ebb38e97b455c5ab2b08b57c
Ivan Ivanoff <iivanoff@thedmsgrp.com>
2018-11-28 16:39:45 -0500

    LL-339: Add history navigation and cache lead data during session

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Analytics from './Analytics';
 import Xverify from './Xverify';
 import TrustedForm from './TrustedForm';
 import Jornaya from './Jornaya';
+import History from './History';
 
 export default {
     override: Override,
     analytics: Analytics,
     xverify: Xverify,
     trustedForm: TrustedForm,
-    jornaya: Jornaya
+    jornaya: Jornaya,
+    history: History
 }
