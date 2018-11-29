74e056c7d1b1a52ad4da78a4bfd8ab3218a79113
wwatson <wwatson@thedmsgrp.com>
2018-11-29 15:08:47 -0500

    Checkbox formatting/spacing

--- src/components/overrides/Checkbox.js
+++ src/components/overrides/Checkbox.js
@@ -7,45 +7,44 @@ export default class CustomCheckboxComponent extends SelectComponent {
      */
     createLabel(container, input)
     {
-            const isLabelHidden = this.labelIsHidden();
-            let className = 'control-label form-check-label';
-            if (this.component.input
-                && !this.options.inputsOnly
-                && this.component.validate
-                && this.component.validate.required) {
-                className += ' field-required';
-            }
+        const isLabelHidden = this.labelIsHidden();
+        let className = 'control-label form-check-label';
+        if (this.component.input
+            && !this.options.inputsOnly
+            && this.component.validate
+            && this.component.validate.required) {
+            className += ' field-required';
+        }
 
-            this.labelElement = this.ce('label', {
-                class: className
-            });
-            this.addShortcut();
+        this.labelElement = this.ce('label', {
+            class: className
+        });
+        this.addShortcut();
 
-            const labelOnTheTopOrOnTheLeft = this.labelOnTheTopOrLeft();
-            if (!isLabelHidden) {
-                // Create the SPAN around the textNode for better style hooks
-                this.labelSpan = this.ce('span');
+        const labelOnTheTopOrOnTheLeft = this.labelOnTheTopOrLeft();
+        if (!isLabelHidden) {
+            // Create the SPAN around the textNode for better style hooks
+            this.labelSpan = this.ce('span');
 
-                if (this.info.attr.id) {
-                    this.labelElement.setAttribute('for', this.info.attr.id);
-                }
+            if (this.info.attr.id) {
+                this.labelElement.setAttribute('for', this.info.attr.id);
             }
-            if (!isLabelHidden && labelOnTheTopOrOnTheLeft) {
-                this.setInputLabelStyle(this.labelElement);
-                this.setInputStyle(input);
-                this.labelSpan.innerHTML = this.component.label;
-                this.labelElement.appendChild(this.labelSpan);
-            }
-            this.addInput(input, this.labelElement);
-            if (!isLabelHidden && !labelOnTheTopOrOnTheLeft) {
-                this.setInputLabelStyle(this.labelElement);
-                this.setInputStyle(input);
-                this.labelSpan.innerHTML = this.addShortcutToLabel();
-                this.labelElement.appendChild(this.labelSpan);
-            }
-            this.createTooltip(this.labelElement);
-            container.appendChild(this.labelElement);
-        };
+        }
+        if (!isLabelHidden && labelOnTheTopOrOnTheLeft) {
+            this.setInputLabelStyle(this.labelElement);
+            this.setInputStyle(input);
+            this.labelSpan.innerHTML = this.component.label;
+            this.labelElement.appendChild(this.labelSpan);
+        }
+        this.addInput(input, this.labelElement);
+        if (!isLabelHidden && !labelOnTheTopOrOnTheLeft) {
+            this.setInputLabelStyle(this.labelElement);
+            this.setInputStyle(input);
+            this.labelSpan.innerHTML = this.addShortcutToLabel();
+            this.labelElement.appendChild(this.labelSpan);
+        }
+        this.createTooltip(this.labelElement);
+        container.appendChild(this.labelElement);
     }
 
 }
