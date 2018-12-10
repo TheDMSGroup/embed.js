5b2ce61d3b369ad7b5f4cc0030d4e0637945cb84
Ivan Ivanoff <iivanoff@thedmsgrp.com>
2018-12-10 15:08:45 -0500

    LL-666: Add custom form builder styling and re-organize archetecture

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Embed from './Embed';
+import Builder from './Builder';
 
 const embed = (options) => new Embed(options);
+const builder = (payload) => new Builder(payload);
 
-export { embed }
+export { embed, builder }
