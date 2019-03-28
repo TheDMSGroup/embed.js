17c2419c3e9d3d11e032c0f325521ac7f882c37f
Ivan Ivanoff <iivanoff@thedmsgrp.com>
2019-03-28 14:13:48 -0400

    Removing custom select.js override and reverting back to native formio select

--- src/components/overrides/index.js
+++ src/components/overrides/index.js
@@ -4,17 +4,13 @@ import Components from 'formiojs/components/Components';
 import CustomButtonComponent from "./Button";
 import ButtonForm from 'formiojs/components/button/Button.form';
 import SliderForm from './Slider.form';
-import SelectForm from 'formiojs/components/select/Select.form';
 import CustomCheckboxComponent from './Checkbox';
-import SubmitSelectComponent from './Select';
 import CustomSliderComponent from './Slider';
 
 const init = () => {
     AllComponents.button = CustomButtonComponent;
     AllComponents.button.editForm = ButtonForm;
     AllComponents.checkbox = CustomCheckboxComponent;
-    AllComponents.select = SubmitSelectComponent;
-    AllComponents.select.editForm = SelectForm;
     AllComponents.slider = CustomSliderComponent;
     AllComponents.slider.editForm = SliderForm;
     Components.setComponents(AllComponents);
